6c93c389e7ceee7a7cd0c0016b414c1a65ba64b9
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2021-12-31 00:04:02 -0500

    Add new function (Article Options)

---
+++ assets/js/article-options.js
@@ -0,0 +1,12 @@
+function openArticleOptions() {
+    document.getElementById('oaos').style.display = 'inherit';
+    document.getElementById('OFFoaos').style.display = 'inherit';
+    document.getElementById('ONoaos').style.display = 'none';
+}
+
+function closeArticleOptions() {
+    document.getElementById('oaos').style.display = 'none';
+    document.getElementById('OFFoaos').style.display = 'none';
+    document.getElementById('ONoaos').style.display = 'inherit';
+
+}
