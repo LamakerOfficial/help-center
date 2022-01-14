b79f9be7bbd271b5702dd2ad2a924e60ef16d1cb
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2022-01-13 22:50:16 -0500

    Update

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,11 +1,12 @@
 function openSearch() {
     document.getElementById('search-searchbar').style.display = 'inherit';
-    document.getElementById('search-searchbar').style.top = '90px';
-    document.getElementById('search-searchbar').style.opacity = '1';
-
     document.getElementById('search-hits').style.display = 'inherit';
-    document.getElementById('search-hits').style.top = '180px';
-    document.getElementById('search-hits').style.opacity = '1';
-
     document.getElementById('cs').innerHTML = "'<style>#bg-art-he, #category, .help-home, .hero, .article, .post, #heroIMG, footer {display: none !important;}'";
+    setTimeout(() => {
+        document.getElementById('search-searchbar').style.top = '90px';
+        document.getElementById('search-searchbar').style.opacity = '1';
+
+        document.getElementById('search-hits').style.top = '180px';
+        document.getElementById('search-hits').style.opacity = '1';
+    }, 0050);
 }
